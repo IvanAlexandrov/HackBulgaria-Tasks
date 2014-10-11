a64b44edcc9ee18da0f940f7aad0ef9d8464c745
IvanAlexandrov <vankel87@gmail.com>
2014-10-11 15:13:15 +0300

    HackBulgaria Tasks

--- week_0/1-Python-simple-problem-set/hack_numbers.py
+++ week_0/1-Python-simple-problem-set/hack_numbers.py
@@ -2,16 +2,14 @@ from int_palindromes import is_int_palindrome
 
 
 def next_hack(n):
-    n <<= 1
-    print(n)
+    n += 1
     binary_n = bin(n)
     binary_n = binary_n[2:]
     ones = binary_n.count("1")
-    # print(is_int_palindrome(int(binary_n)), ones)
-    if is_int_palindrome(int(binary_n)) and ones % 2 != 0:
+    if is_int_palindrome(binary_n) and ones % 2 != 0:
+        print(n)
         return n
-    return next_hack(n + 1)
-
+    next_hack(n)
 
 print(next_hack(0))
 print(next_hack(10))
