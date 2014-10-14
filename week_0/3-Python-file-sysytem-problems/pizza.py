03595d95e688d90738d2b01ca81039b08d9c44f4
IvanAlexandrov <vankel87@gmail.com>
2014-10-14 20:01:12 +0300

    Thirs Tasks Set

--- week_0/3-Python-file-sysytem-problems/pizza.py
+++ week_0/3-Python-file-sysytem-problems/pizza.py
@@ -1,54 +1,162 @@
+import os
+from time import time
+from datetime import datetime
+order_dict = {}
+
+
 def take(name, price):
-    pass
+    price = float(price)
+    if name in order_dict:
+        order_dict[name] += price
+    else:
+        order_dict[name] = price
+    print("Taking order from %s for %.2f" % (name, price))
+    return order_dict
 
 
-def status():
-    pass
+def status(orders):
+    for client in orders.keys():
+        print(client + " - %.2f" % float(orders[client]))
+    return True
 
 
-def save():
-    pass
+def save(orders):
+    ts = time()
+    stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+    orders_list = open("orders.txt", "a+")
+    num_lines = sum(1 for line in open("orders.txt"))
+    orders_list.write('[' + str(num_lines + 1) + ']' +
+                      ' - orders_' + stamp + '\n'
+                      )
+    orders_list.close()
+
+    fp = open("orders_" + stamp + '.txt', "w")
+    for client in orders.keys():
+        fp.write(client + " - %.2f" % orders[client] + '\n')
+    fp.close()
+    orders.clear()
+    print("Saved the current order to" + "orders_" + stamp + '.txt')
+    return True
 
 
 def list_():
-    pass
+    if os.path.exists("orders.txt"):
+        fp = open("orders.txt", "r")
+        print(fp.read())
+        fp.close()
+        return True
+    return False
 
 
-def load(number):
-    pass
+def load(command):
+    order = command.split(" ")
+    index = '[' + order[1] + ']'
+    file_name = ''
+    fp = open("orders.txt", "r")
 
+    for line in fp:
+        if line.startswith(index):
+            file_name = line.split(" - ")[1].rstrip("\n")
+    fp.close()
 
-def finish():
-    pass
+    print("Loading orders " + file_name)
+    fp = open(file_name + ".txt", "r")
+    content = []
+
+    for line in fp:
+        content += line.rstrip("\n").split(" - ")
+    fp.close()
+
+    names = content[::2]
+    prices = content[1::2]
+    orders = dict(zip(names, prices))
+
+    return orders
 
 
 def main():
-    print(
-        "Yuo can select one of the options below:\n"
-        "\ttake <name> <price>\n"
-        "\tstatus\n"
-        "\tsave\n"
-        "\tlist\n"
-        "\tload <number>\n"
-        "\tfinish"
-        )
-    while True:
-        user_input = raw_input("Enter a command> ")
-        if (user_input.startswith("take")):
-            params = user_input.split()
-            print("Taking order from %s for %d" %(params[1], params[2]))
-            take(params[1], params[2])
-        elif ((user_input == 'status')):
-            status()
-        elif ((user_input == save)):
-            save()
-        elif ((user_input == 'list')):
+    print("Enter one of the following commands:\n"
+          "take <name> <price> - place your order\n"
+          "status - check the order status \n"
+          "save - save order\n"
+          "list - shows saved orders\n"
+          "load <number> - load a saved order\n"
+          "finish - exit the program")
+
+    command = input("Enter your command> ")
+    orders = {}
+    is_saved = False
+    is_empty = True
+    is_printed = False
+
+    discard = False
+
+    while command != 'finish':
+        discard = False
+        if command.startswith('take'):
+            order = command.split(" ")
+            name = order[1]
+            price = order[2]
+            order = take(name, price)
+            orders[name] = order[name]
+            is_saved = False
+            is_empty = False
+        elif (command == 'status'):
+            status(orders)
+
+        elif command == 'save':
+            is_saved = True
+            is_printed = False
+            save(orders)
+
+        elif command == 'list':
             list_()
-        elif (user_input == 'finish'):
-            finish()
+            is_printed = True
+
+        elif command.startswith('load'):
+            if is_printed is False:
+                print("Use list command before loading")
+
+            elif is_empty is True:
+                orders = load(command)
+
+            elif is_saved is False and discard is False:
+                discard = True
+                print("You have not saved the current order.\n"
+                      "If you wish to discard it, type load <number> again.")
+
+            elif is_saved is False and discard is True:
+                orders.clear()
+                orders = load(command)
+
         else:
-            print("Invalid option")
-    print(user_input)
+            print("Unknown command! \n"
+                  "Try one of the following:\n"
+                  "take <name> <price>\n"
+                  "status\n"
+                  "save\n"
+                  "list\n"
+                  "load <number>\n"
+                  "finish\n"
+                  )
+
+        command = input("Enter your command> ")
+
+    else:
+        if is_saved is False and is_empty is False:
+            print("You have not saved your order.\n"
+                  "If you wish to continue, type finish again.\n"
+                  "If you want to save your order, type save\n"
+                  )
+            command = input("Enter your command> ")
+            if command == 'save':
+                save(orders)
+                print("Your order has been saved. Goodbye!")
+            elif command == 'finish':
+                print("Finishing order. Goodbye!")
+        else:
+            exit()
+
 
 if __name__ == '__main__':
     main()
